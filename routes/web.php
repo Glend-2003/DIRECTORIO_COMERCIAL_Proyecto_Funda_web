459018989c716c5665c6390d12ce6ac94c0f7dfa
Jamel Sandí <jsandi12199@gmail.com>
2025-10-27 23:33:43 -0600

    Commerce CRUD
    The backend of this module is ready, all the endpoints work well, the view is not ready yet.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para el recurso Comercio
+//Route::resource('comercios', App\Http\Controllers\ComercioController::class);   
+Route::get('/debug-comercios', function () {
+    $comercios = \App\Models\Comercio::all();
+    
+    dd([
+        'total' => $comercios->count(),
+        'comercios' => $comercios->toArray()
+    ]);
+});
