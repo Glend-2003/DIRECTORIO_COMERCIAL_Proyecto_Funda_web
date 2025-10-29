b6ce4482e4faa52f04bc7aa5b0716bb1a56b4c89
Jeremy Romero Carazo <117392636+Romero42@users.noreply.github.com>
2025-10-29 11:20:12 -0600

    Added frontend like comercio
    Added buttons and modals with the styles of crud comercio for better user experience

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     $totalComercios = \App\Models\Comercio::where('NUM_ESTADO', 1)->count();
     $comerciosRecientes = \App\Models\Comercio::where('NUM_ESTADO', 1)
-                                            ->latest()
-                                            ->take(5)
-                                            ->get();
+        ->latest()
+        ->take(5)
+        ->get();
     return view('admin.dashboard', compact('totalComercios', 'comerciosRecientes'));
 })->middleware('auth')->name('dashboard');
 
@@ -24,10 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('comercios', ComercioController::class);
+    Route::resource('sliders', ControllerSlider::class);
 });
 
-require __DIR__.'/auth.php';
-
-Route::resource('comercios', ComercioController::class);
-
-Route::resource('sliders', ControllerSlider::class);
+require __DIR__ . '/auth.php';
